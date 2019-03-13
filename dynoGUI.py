76d636ef1719bab9f7fa67cdf45c1545a599b46a
Sandra Kue <sandrakue08@gmail.com>
2019-03-13 00:10:20 -0400

    Fixed aesthetics and added dino

--- dynoGUI.py
+++ dynoGUI.py
@@ -1,22 +1,25 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Feb 26 14:48:21 2019
+The GUI
 
-@author: Sandra
+
+
+
 """
 
 from guizero import App, Text, TextBox, PushButton, Picture
 from dynamometer import dynamometer
 
-
+# This function updates the diameter
 def updateD():
     dynodiameter = input_diameter.value
     mydyno.updateWheel(dynodiameter)
     
+# This function updates the horsepower
 def updateH():
     dynohorsepower = input_horsepower.value
     mydyno.updateHorse(dynohorsepower)
-    
+
+# This function displays the values of known variables    
 def displayValues():
     one = Text(app, text="diammeter: " + str(mydyno.wheelDiameter), grid=[1,7], align="left")
     two = Text(app, text="horsepower: " + str(mydyno.horsepower), grid=[1,8], align="left")
@@ -27,27 +30,29 @@ def displayValues():
     mydyno.calculate()    
     five = Text(app, text="torque: " + str(mydyno.torque), grid=[1,11], align="left")
 
-app = App(title="Dyno Testing", width=500,height=500,layout="grid")
 
-dinopicture = Picture(app, image="dinosaur.jpg", grid=[0,0])
 
-welcome_message = Text(app, text="Welcome to the Dyno GUI",size=30, color="green", grid=[1,0], width="fill", align="center")
 
-dinopicture2 = Picture(app, image="dinosaur.jpg", grid=[2,0])
+app = App(title="Dyno Testing", width=610,height=500,layout="grid", bg="#fdbbff")
+
+dinopicture = Picture(app, image="dinosaur1.gif", grid=[0,0])
+
+welcome_message = Text(app, text="Welcome to the Dyno GUI",size=30, color="blue", grid=[1,0], width="fill")
+dinopicture2 = Picture(app, image="dinosaur1.gif", grid=[2,0])
 """
 Diameter
 """
 d_description = Text(app, text="Diameter (mm): ", grid=[1,1], align="left")
-input_diameter = TextBox(app, grid=[1,1], width=10, align="center")
-update_diameter = PushButton(app, command=updateD, text="Update Diameter",grid=[1,2],align="center" )
+input_diameter = TextBox(app, grid=[1,1], width=10)
+update_diameter = PushButton(app, command=updateD, text="Update Diameter",grid=[1,2] )
 
 
 """
 Horsepower
 """
 h_description = Text(app, text="Horsepower (hp): ", grid=[1,4], align="left")
-input_horsepower = TextBox(app, grid=[1,4], width=10, align="center")
-update_horsepower = PushButton(app, command=updateH, text="Update Horsepower", grid=[1,5],align="center" )
+input_horsepower = TextBox(app, grid=[1,4], width=10)
+update_horsepower = PushButton(app, command=updateH, text="Update Horsepower", grid=[1,5],align="top" )
 
 
 """
