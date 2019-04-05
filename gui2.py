74846cad873e666540b190f9b7660f0e3c595766
phamtim1 <phamtim1@msu.edu>
2019-04-05 13:09:39 -0400

    reformat code

--- gui2.py
+++ gui2.py
@@ -1,9 +1,5 @@
 """
 The GUI
-
-
-
-
 """
 
 from guizero import App, Text, TextBox, PushButton, Picture
@@ -12,18 +8,17 @@ from tacho_reader import HallEffectReader, calculate_speed
 
 
 class dynoGUI:
-    def __init__(self, d):
+    def __init__(self, dyno):
+        self.mydyno = dyno
+        
         self.app = App(title="Dyno Testing", width=610, height=500,
             layout="grid", bg="#fdbbff")
         
-        welcome_message = Text(self.app, text="Welcome to the Dyno GUI",
+        welcome_msg = Text(self.app, text="Welcome to the Dyno GUI",
            size=30, color="blue", grid=[1,0], width="fill")
 
         dinopicture = Picture(self.app, image="dinosaur1.gif", grid=[0,0])
-        dinopicture2 = Picture(self.app, image="dinosaur1.gif", grid=[2,0])
-        
-        self.mydyno = d
-        
+        dinopicture = Picture(self.app, image="dinosaur1.gif", grid=[2,0])
         
         
     # This function updates the diameter
@@ -43,14 +38,22 @@ class dynoGUI:
 
     # This function displays the values of known variables    
     def displayValues(self):
-        one = Text(self.app, text="diammeter: " + str(mydyno.wheelDiameter), grid=[1,7], align="left")
-        two = Text(self.app, text="horsepower: " + str(mydyno.horsepower), grid=[1,8], align="left")
-        three = Text(self.app, text="rpm: " + str(mydyno.rpm), grid=[1,9], align="left")
-        four = Text(self.app, text="speed: " + str(mydyno.speed), grid=[1,10], align="left")
+        one = Text(self.app, text="diammeter: " + str(mydyno.wheelDiameter),
+            grid=[1,7], align="left")
+            
+        two = Text(self.app, text="horsepower: " + str(mydyno.horsepower),
+            grid=[1,8], align="left")
+            
+        three = Text(self.app, text="rpm: " + str(mydyno.rpm), grid=[1,9],
+            align="left")
+            
+        four = Text(self.app, text="speed: " + str(mydyno.speed), grid=[1,10],
+            align="left")
         
-        #calculates the twerk
-        #mydyno.calculate()    
-        #five = Text(self.app, text="torque: " + str(mydyno.torque), grid=[1,11], align="left")
+        # calculates the twerk
+        # mydyno.calculate()
+        # five = Text(self.app, text="torque: " + str(mydyno.torque), grid=[1,11],
+        #    align="left")
 
 
 
@@ -58,27 +61,36 @@ class dynoGUI:
         """
         Diameter
         """
-        d_description = Text(self.app, text="Diameter (mm): ", grid=[1,1], align="left")
+        d_description = Text(self.app, text="Diameter (mm): ", grid=[1,1],
+            align="left")
+            
         input_diameter = TextBox(self.app, grid=[1,1], width=10)
-        update_diameter = PushButton(self.app, command=self.updateD ,text="Update Diameter",
-            grid=[1,2])
-        update_diameter.update_command(command=self.updateD, args=[input_diameter])
-        
+        update_diameter = PushButton(self.app, command=self.updateD ,
+            text="Update Diameter", grid=[1,2])
+            
+        update_diameter.update_command(command=self.updateD,
+            args=[input_diameter])
         
         """
         Horsepower
         """
         h_description = Text(self.app, text="Horsepower (hp): ", grid=[1,4],
             align="left")
+            
         input_horsepower = TextBox(self.app, grid=[1,4], width=10)
+        
         update_horsepower = PushButton(self.app, command=self.updateH,
             text="Update Horsepower", grid=[1,5],align="top" )
-        update_horsepower.update_command(command=self.updateH, args=[input_horsepower])
+        update_horsepower.update_command(command=self.updateH,
+            args=[input_horsepower])
         
-        show_info = PushButton(self.app,command=self.displayValues, text= "Calculate Results", grid=[1,6])
+        show_info = PushButton(self.app,command=self.displayValues,
+            text= "Calculate Results", grid=[1,6])
 
 
-
+        """
+        Tachometer
+        """
         get_tacho = PushButton(self.app, command=self.mydyno.enable_tacho,
             text="Enable Tachometer", grid=[1,7])
 
@@ -91,7 +103,6 @@ class dynoGUI:
         self.app.display()
 
 if __name__ == "__main__":
-
     """
     Initiates the dyno
     """
